2fd82700d4955c6994e7d602aa1c774bdb077f2b
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-28 23:34:02 +0200

    Third weekend project

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,36 +2,63 @@ var textArea = document.getElementById("textArea");
 var str = "";
 var capsValue = false;
 var value = "";
+var shift = false;
 
-if(document.getElementById('caps-lock').clicked == true) {
-       alert("clicked");
-     };
+if (document.getElementById("caps-lock").clicked == true) {
+  alert("clicked");
+}
+
 function btnClick(id) {
   value = document.getElementById(id).innerHTML;
+  if (capsValue == true) {
+    if (shift == true) {
+      textArea.innerHTML += value;
+      shift = false;
+    } else {
+      textArea.innerHTML += value.toUpperCase();
+    }
+  } else {
+    if (shift == true) {
+      textArea.innerHTML += value.toUpperCase();
+      shift = false;
+    } else {
+      textArea.innerHTML += value;
+    }
+  }
 
-  textArea.innerHTML += value;
   str = textArea.innerHTML;
 }
- function capsLock(value) {
-     capsValue = true;
-    //textArea.innerHTML -= capsValue;
-    textArea.innerHTML += value.toUpperCase();
+
+function capsLock() {
+  if (capsValue == false) {
+    capsValue = true;
+  } else {
+    capsValue = false;
+  }
+}
+
+function shiftf() {
+  if (shift == false) shift = true;
 }
 
 function btnOk() {
   alert(textArea.innerHTML);
 }
+
 function backSpace() {
   var newStr = "";
-  //var textArea = document.getElementById("textArea").innerHTML;
   for (var i = 0; i < str.length - 1; i++) {
     newStr += str[i];
   }
   str = newStr;
-
   textArea.innerHTML = "";
   textArea.innerHTML = newStr;
 }
+
 function space() {
   textArea.innerHTML += " ";
+}
+
+function enter() {
+  textArea.innerHTML += "\n";
 }
